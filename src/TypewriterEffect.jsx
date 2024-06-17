044069c95e51bcfb2eddd60df9030ce0b7645ccc
Corina Udrescu <corinutz@gmail.com>
2024-06-17 07:50:49 +0200

    no longer re-create the interval

--- src/TypewriterEffect.jsx
+++ src/TypewriterEffect.jsx
@@ -1,34 +1,50 @@
-import { useEffect } from "react";
-import { useState } from "react";
+import { useCallback, useEffect, useRef, useState } from "react";
 
 const TypewriterEffect = () => {
-  const [sentence, setSentence] = useState("");
   const [typedSentence, setTypedSentence] = useState("");
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    const data = new FormData(e.target);
-    setSentence(data.get("sentence"));
+  const [typewriterIntervalId, setTypewriterIntervalId] = useState(null);
+  const savedCallback = useRef();
+
+  // start the interval and save its id,
+  // so we can stop it once all letters are typed
+  const startTyping = (sentence) => {
     setTypedSentence("");
+    const typewriterIntervalId = setInterval(() => {
+      savedCallback.current(sentence);
+    }, 500);
+    setTypewriterIntervalId(typewriterIntervalId);
   };
 
-  useEffect(() => {
-    if (sentence === "") {
-      return;
-    }
-    const letters = Array.from(sentence);
-    const typewriterInterval = setInterval(() => {
+  // stop the interval
+  const stopTyping = useCallback(() => {
+    clearInterval(typewriterIntervalId);
+    setTypewriterIntervalId(null);
+  }, [typewriterIntervalId]);
+
+  const typeNextLetter = useCallback(
+    (sentence) => {
+      const letters = Array.from(sentence);
       if (typedSentence.length === letters.length) {
         // we typed all the letters
-        clearInterval(typewriterInterval);
+        stopTyping();
         return;
       }
       // else, type next letter
       const nextLetterIndex = typedSentence.length;
       setTypedSentence(typedSentence + letters[nextLetterIndex]);
-    }, 500);
+    },
+    [typedSentence, stopTyping]
+  );
 
-    return () => clearInterval(typewriterInterval);
-  }, [sentence, typedSentence]);
+  useEffect(() => {
+    savedCallback.current = typeNextLetter;
+  }, [typeNextLetter]);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const data = new FormData(e.target);
+    startTyping(data.get("sentence"));
+  };
 
   return (
     <div>
