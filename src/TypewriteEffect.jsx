68b16ed60a3b121649299e4614082ec9b37cb34f
Corina Udrescu <corinutz@gmail.com>
2024-06-17 07:17:58 +0200

    working solution

--- src/TypewriteEffect.jsx
+++ src/TypewriteEffect.jsx
@@ -1,10 +1,35 @@
+import { useEffect } from "react";
+import { useState } from "react";
+
 const TypewriterEffect = () => {
+  const [sentence, setSentence] = useState("");
+  const [typedSentence, setTypedSentence] = useState("");
   const handleSubmit = (e) => {
     e.preventDefault();
     const data = new FormData(e.target);
-    // TODO Display the text with typewriter effect
-    console.log(`The sentence to display is ${data.get("sentence")}`);
+    setSentence(data.get("sentence"));
+    setTypedSentence("");
   };
+
+  useEffect(() => {
+    if (sentence === "") {
+      return;
+    }
+    const letters = Array.from(sentence);
+    const typewriterInterval = setInterval(() => {
+      if (typedSentence.length === letters.length) {
+        // we typed all the letters
+        clearInterval(typewriterInterval);
+        return;
+      }
+      // else, type next letter
+      const nextLetterIndex = typedSentence.length;
+      setTypedSentence(typedSentence + letters[nextLetterIndex]);
+    }, 500);
+
+    return () => clearInterval(typewriterInterval);
+  }, [sentence, typedSentence]);
+
   return (
     <div>
       <form
@@ -22,6 +47,7 @@ const TypewriterEffect = () => {
         />
         <button type="submit">Display with typewriter effect</button>
       </form>
+      {typedSentence && <p>You typed {typedSentence}</p>}
     </div>
   );
 };
